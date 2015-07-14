51b9f79b3b9de015f2404e7f12edf45700e2a059
Praveen Chavan <pchavan@codeaurora.org>
2015-07-14 12:38:37 -0700

    CamcorderProfiles: Add new profiles for recorder
    Below changes are squased:
    
    video: Add QHD & 2K profile for recorder
    
    add VGA and 4K-DCI profiles.
    
    Change-Id: I4d23d7609225e921f6d0500bb03756bbfb2cde21
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -485,6 +485,7 @@ void JNICameraContext::setCallbackMode(JNIEnv *env, bool installed, bool manualM
 static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jboolean enable)
 {
     ALOGV("setLongshot");
+#ifdef QCOM_HARDWARE
     JNICameraContext* context;
     status_t rc;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -499,6 +500,7 @@ static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jbool
     if (rc != NO_ERROR) {
        jniThrowException(env, "java/lang/RuntimeException", "enabling longshot mode failed");
     }
+#endif
 }
 
 static void android_hardware_Camera_stopLongshot(JNIEnv *env, jobject thiz)
@@ -519,6 +521,7 @@ static void android_hardware_Camera_stopLongshot(JNIEnv *env, jobject thiz)
 static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  {
    ALOGV("sendHistogramData" );
+#ifdef QCOM_HARDWARE
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -529,10 +532,12 @@ static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
    if (rc != NO_ERROR) {
       jniThrowException(env, "java/lang/RuntimeException", "send histogram data failed");
     }
+#endif
  }
  static void android_hardware_Camera_setHistogramMode(JNIEnv *env, jobject thiz, jboolean mode)
  {
    ALOGV("setHistogramMode: mode:%d", (int)mode);
+#ifdef QCOM_HARDWARE
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -546,6 +551,7 @@ static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
    if (rc != NO_ERROR) {
       jniThrowException(env, "java/lang/RuntimeException", "set histogram mode failed");
      }
+#endif
  }
 void JNICameraContext::addCallbackBuffer(
         JNIEnv *env, jbyteArray cbb, int msgType)
