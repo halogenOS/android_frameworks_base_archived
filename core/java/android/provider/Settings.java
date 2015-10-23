06bb707029c93f5f9461859ddb64fca56df032a6
Lars Greiss <kufikugel@googlemail.com>
2015-10-23 19:56:01 +0000

    Add reboot menu [1/2]
    - Adapt for halogenOS 7.0
    - prepare for Global menu
    - misc changes
    
    Adding advanced reboot menu like it was on kk4.4
    Adding advanced reboot secure option
    Move reboot into safe mode from longpress power to longpress reboot
    Enable for now per default Airplane mode and sound and reboot till we have imported our SlimActions class to make in configurable
    
    Misc changes and adaptions to LP, M, and now N
    
    Change-Id: I97692bc15e3d0d9962b53b6485b2081c6d6948b3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6121,6 +6121,12 @@ public final class Settings {
                 INCALL_POWER_BUTTON_BEHAVIOR_SCREEN_OFF;
 
         /**
+         * Whether to include options in power menu for rebooting into recovery and bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Whether the device should wake when the wake gesture sensor detects motion.
          * @hide
          */
@@ -6523,6 +6529,7 @@ public final class Settings {
             TTY_MODE_ENABLED,
             INCALL_POWER_BUTTON_BEHAVIOR,
             WIFI_DISCONNECT_DELAY_DURATION
+            ADVANCED_REBOOT,
         };
 
         /**
