035b44742b86fd95a7c6c64edb288f570a7f54b2
dhacker29 <dhackerdvm@gmail.com>
2015-11-24 01:53:47 -0500

    fw/b: Use ro.build.version.incremental to signal OTA upgrades
    Port to 14
    
    Squash of:
    
    Author: dhacker29 <dhackerdvm@gmail.com>
    Date:   Tue Nov 24 01:53:47 2015 -0500
        Core: Use ro.build.date to signal mIsUpgrade
    
        M: We use a static fingerprint that is only changed when a new OEM build is released, so
           every flash shows Android is starting instead of upgrading.  This will fix that.
        N: even though we dont have the dexopt sceen on N, this is still needed to delete the
           correct caches, and grant/deny specific runtime permissions like a true oem update
           would do.
           Updated for Nougat By: BeansTown106
    
        Change-Id: I0e3ed5c8f0351e48944432ae6a0c5194ddeff1fa
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 28 13:45:00 2018 -0700
        fw/b UserManagerService: Use ro.build.date to signal upgrades
    
        *) We changed PackageManagerService to use Build.DATE instead of
           Build.FINGERPRINT to detect upgrade.  Do the same for
           UserManagerService.
        *) Affects generation of preboot intent and app data migration.
    
        Change-Id: I56887b7ca842afdcf3cf84b27b4c04667cf43307
    
    Author: Wang Han <416810799@qq.com>
    Date:   Sat Dec 29 23:33:20 2018 +0800
        ShortcutService: Use ro.build.date to signal package scanning
    
         * Affects system apps scanning.
    
        Change-Id: I5f6d6647929f5b5ae7e820b18e95bf5ed2ec8d1c
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Tue Nov 19 01:02:01 2019 +0100
        base: Use ro.build.date to clear cache dirs on update
    
        instead of using ro.build.fingerprint we explictly need to use ro.build.date
    
        Change-Id: Ib3e80e58eb8c9a21c108e9f5cd2dbdb7ada8e3a4
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed Oct 28 07:07:10 2020 -0400
        One more Build.FINGERPRINT to Build.DATE change
    
        Change-Id: I13dbf3d7f6587d3fcd6591cc0f861b34b6d5561c
    
    Change-Id: If0eb969ba509981f9209ffa37a949d9042ef4c2a
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -4876,7 +4876,7 @@ public class UserManagerService extends IUserManager.Stub {
                     userInfo.creationTime = getCreationTime();
                     userInfo.partial = true;
                     userInfo.preCreated = preCreate;
-                    userInfo.lastLoggedInFingerprint = PackagePartitions.FINGERPRINT;
+                    userInfo.lastLoggedInFingerprint = Build.VERSION.INCREMENTAL;
                     if (userTypeDetails.hasBadge() && parentId != UserHandle.USER_NULL) {
                         userInfo.profileBadge = getFreeProfileBadgeLU(parentId, userType);
                     }
@@ -6200,7 +6200,7 @@ public class UserManagerService extends IUserManager.Stub {
         t.traceBegin("onBeforeStartUser-" + userId);
         final int userSerial = userInfo.serialNumber;
         // Migrate only if build fingerprints mismatch
-        boolean migrateAppsData = !PackagePartitions.FINGERPRINT.equals(
+        boolean migrateAppsData = !Build.VERSION.INCREMENTAL.equals(
                 userInfo.lastLoggedInFingerprint);
         t.traceBegin("prepareUserData");
         mUserDataPreparer.prepareUserData(userId, userSerial, StorageManager.FLAG_STORAGE_DE);
@@ -6231,7 +6231,7 @@ public class UserManagerService extends IUserManager.Stub {
         }
         final int userSerial = userInfo.serialNumber;
         // Migrate only if build fingerprints mismatch
-        boolean migrateAppsData = !PackagePartitions.FINGERPRINT.equals(
+        boolean migrateAppsData = !Build.VERSION.INCREMENTAL.equals(
                 userInfo.lastLoggedInFingerprint);
 
         final TimingsTraceAndSlog t = new TimingsTraceAndSlog();
@@ -6276,7 +6276,7 @@ public class UserManagerService extends IUserManager.Stub {
         if (now > EPOCH_PLUS_30_YEARS) {
             userData.info.lastLoggedInTime = now;
         }
-        userData.info.lastLoggedInFingerprint = PackagePartitions.FINGERPRINT;
+        userData.info.lastLoggedInFingerprint = Build.VERSION.INCREMENTAL;
         scheduleWriteUser(userId);
     }
 
