7b1735daf301a08438166bd10e0bb7ba2a2b46ff
Christopher Tate <ctate@google.com>
2016-06-13 15:17:54 -0700

    Don't trust callers to supply app info to bindBackupAgent()
    Get the canonical identity and metadata about the package from the
    Package Manager at time of usage rather than rely on the caller to
    have gotten things right, even when the caller has the system uid.
    
    Bug 28795098
    
    Change-Id: I215786bc894dedf7ca28e9c80cefabd0e40ca877
    
    Merge conflict resolution for ag/1133474 (referencing ag/1148862)
    - directly to mnc-mr2-release

--- services/backup/java/com/android/server/backup/BackupManagerService.java
+++ services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -2231,11 +2231,12 @@ public class BackupManagerService {
     // fire off a backup agent, blocking until it attaches or times out
     IBackupAgent bindToAgentSynchronous(ApplicationInfo app, int mode) {
         IBackupAgent agent = null;
-        try {
-            synchronized(mAgentConnectLock) {
-                mConnecting = true;
-                mConnectedAgent = null;
-                if (mActivityManager.bindBackupAgent(app, mode)) {
+        synchronized(mAgentConnectLock) {
+            mConnecting = true;
+            mConnectedAgent = null;
+            try {
+                if (mActivityManager.bindBackupAgent(app.packageName, mode,
+                        UserHandle.USER_OWNER)) {
                     Slog.d(TAG, "awaiting agent for " + app);
 
                     // success; wait for the agent to arrive
