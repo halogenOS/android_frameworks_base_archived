b37f86f20c28a795a8af2d435f5c8fdc5e619c62
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-25 22:00:01 +0100

    cmds: add oms command
    Change-Id: Id336e19a4ca9ff3048b28836c36bef9476c4d5f0

---
+++ cmds/oms/Android.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2016 halogenOS
+#
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := oms
+LOCAL_SRC_FILES := oms
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+
