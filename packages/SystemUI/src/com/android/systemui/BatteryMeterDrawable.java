36215f23fd6ffe384b3a0b0961bf340e968dcb5c
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-25 09:08:12 -0500

    Battery icon: let disable force text outside icon when charging [1/2]
    When text outside the icon is disabled, remove the bolt and put the text
    inside the icon.
    
    Also cleanup the code a bit.
    
    Change-Id: Ib7b079c8a0a6efbea184943f4a7f1fb945272885
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -56,6 +56,8 @@ public class BatteryMeterDrawable extends Drawable implements
     public static final String TAG = BatteryMeterDrawable.class.getSimpleName();
     public static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
             Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT;
+    private static final String FORCE_CHARGE_BATTERY_TEXT =
+            Settings.Secure.FORCE_CHARGE_BATTERY_TEXT;
 
     private static final boolean SINGLE_DIGIT_PERCENT = false;
 
@@ -117,6 +119,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private int mLevel = -1;
     private boolean mPluggedIn;
+    private boolean mForceChargeBatteryText;
     private boolean mListening;
     private static final int ADD_LEVEL = 10;
     private static final int ANIM_DURATION = 500;
@@ -179,6 +182,7 @@ public class BatteryMeterDrawable extends Drawable implements
         levels.recycle();
         colors.recycle();
         updateShowPercent();
+        updateForceChargeBatteryText();
         mWarningString = context.getString(R.string.battery_meter_very_low_overlay_symbol);
         mCriticalLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -233,7 +237,6 @@ public class BatteryMeterDrawable extends Drawable implements
 
         mIntrinsicWidth = context.getResources().getDimensionPixelSize(R.dimen.battery_width);
         mIntrinsicHeight = context.getResources().getDimensionPixelSize(R.dimen.battery_height);
-        
         mLessHeight = Math.round(dp2px(1, context) / 4);
     }
 
@@ -250,9 +253,13 @@ public class BatteryMeterDrawable extends Drawable implements
     public void startListening() {
         mListening = true;
         mContext.getContentResolver().registerContentObserver(
-                Settings.Secure.getUriFor(Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT),
+                Settings.Secure.getUriFor(STATUS_BAR_SHOW_BATTERY_PERCENT),
+                false, mSettingObserver);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(FORCE_CHARGE_BATTERY_TEXT),
                 false, mSettingObserver);
         updateShowPercent();
+        updateForceChargeBatteryText();
         mBatteryController.addStateChangedCallback(this);
     }
 
@@ -335,7 +342,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private void updateShowPercent() {
         mShowPercent = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, 0) == 1;
+                STATUS_BAR_SHOW_BATTERY_PERCENT, 0) == 1;
     }
 
     private int getColorForLevel(int percent) {
@@ -649,7 +656,7 @@ public class BatteryMeterDrawable extends Drawable implements
         final int level = mLevel;
 
         mTextAndBoltPaint.setColor(getColorForLevel(level));
-
+        updateForceChargeBatteryText();
         // Make sure we don't draw the charge indicator if not plugged in
         final Drawable d = mBatteryDrawable.findDrawableByLayerId(R.id.battery_charge_indicator);
         if (d instanceof BitmapDrawable) {
@@ -657,12 +664,12 @@ public class BatteryMeterDrawable extends Drawable implements
             // happened, we need to change the paint rather than the alpha in case the blendMode
             // has been set to clear.  Clear always clears regardless of alpha level ;)
             final BitmapDrawable bd = (BitmapDrawable) d;
-            bd.getPaint().set(mPluggedIn ? mTextAndBoltPaint : mClearPaint);
+            bd.getPaint().set(mPluggedIn && mForceChargeBatteryText ? mTextAndBoltPaint : mClearPaint);
             if (mBoltOverlay) {
                 mBoltDrawable.setTint(getBoltColor());
             }
         } else {
-            d.setAlpha(mPluggedIn ? 255 : 0);
+            d.setAlpha(mPluggedIn && mForceChargeBatteryText ? 255 : 0);
         }
 
         // Now draw the level indicator
@@ -673,7 +680,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
         // If chosen by options, draw percentage text in the middle
         // Always skip percentage when 100, so layout doesnt break
-        if (!mPluggedIn) {
+        if (!mPluggedIn || (mPluggedIn && !mForceChargeBatteryText)) {
             drawPercentageText(canvas);
         }
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -70,6 +70,7 @@ public class KeyguardStatusBarView extends RelativeLayout
 
     private boolean mShowBatteryText;
     private boolean mForceBatteryText;
+    private boolean mForceChargeBatteryText;
 
     public KeyguardStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -174,9 +175,8 @@ public class KeyguardStatusBarView extends RelativeLayout
                 mMultiUserSwitch.setVisibility(View.GONE);
             }
         }
-
         mBatteryLevel.setVisibility(
-                mBatteryCharging || mShowBatteryText || mForceBatteryText ? View.VISIBLE : View.GONE);
+                (mBatteryCharging && mForceChargeBatteryText) || mShowBatteryText || mForceBatteryText ? View.VISIBLE : View.GONE);
     }
 
     private void updateSystemIconsLayoutParams() {
@@ -341,6 +341,8 @@ public class KeyguardStatusBarView extends RelativeLayout
             mShowBatteryText = newValue == null ? false : Integer.parseInt(newValue) == 2;
             mForceBatteryText = Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0) == 6 ? true : false;
+            mForceChargeBatteryText = Settings.Secure.getInt(getContext().getContentResolver(),
+                    Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 1) == 1 ? true : false;
             updateVisibilities();
         }
     }
