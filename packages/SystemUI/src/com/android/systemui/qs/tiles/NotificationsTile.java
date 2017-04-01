06bc35a506c74b3e74386d2529c4316ee2f88a6e
Harsh Shandilya <msfjarvis@gmail.com>
2017-04-01 21:17:13 +0530

    [QUICKSETTINGS] Add notifications tile
    Ticker icon from https://github.com/ResurrectionRemix/Resurrection_packages_apps_Settings/commit/0af9453e5659b1b7203ffb3d571859a4ebf78c1e
    
    Heads-up icons from https://github.com/LineageOS/android_packages_apps_CustomTiles/commit/3433194ce312164fd54cbb3fa96a33064b6a3b6f
    
    Change-Id: I4221be1d71bf5d7dbca4c0ed861451a29ea73002

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NotificationsTile.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2017 The halogenOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class NotificationsTile extends QSTile<QSTile.State> {
+
+    private String mNotif = "";
+
+
+    public NotificationsTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    public State newTileState() {
+        return new QSTile.State();
+    }
+
+    @Override
+    public void handleClick() {
+        switch(mNotif){
+            case "":
+                mNotif = Settings.System.STATUS_BAR_SHOW_TICKER;
+                break;
+            case Settings.System.STATUS_BAR_SHOW_TICKER:
+                mNotif = Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS;
+                break;
+            case Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS:
+                mNotif = "";
+                break;
+        }
+        refreshState();
+    }
+
+    @Override
+    public void handleLongClick() {
+        Intent intent = new Intent("android.settings.CUSTOMIZATIONS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        mHost.startActivityDismissingKeyguard(intent);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_notifications_none_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    protected void handleUpdateState(QSTile.State state, Object arg) {
+        switch (mNotif){
+            case "":
+                state.label = mContext.getString(R.string.quick_settings_notifications_none_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_notifications_headsup_off);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS,0);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER,0);
+                break;
+            case Settings.System.STATUS_BAR_SHOW_TICKER:
+                state.label = mContext.getString(R.string.quick_settings_notifications_ticker_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_notifications_ticker);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS,0);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER,1);
+                break;
+            case Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS:
+                state.label = mContext.getString(R.string.quick_settings_notifications_headsup_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_notifications_headsup_on);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS,1);
+                Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER,0);
+                break;
+        }
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+    }
+}
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -59,6 +59,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -467,6 +468,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("ime")) return new ImeTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
         else if (tileSpec.equals("customizations")) return new CustomizationsTile(this);
+        else if (tileSpec.equals("notifications")) return new NotificationsTile(this);
         else if (tileSpec.equals(mImageTileSpec)) {
             if (PackageUtils.isImageTileInstalled(mContext)) {
                 return new ImageTile(this);
