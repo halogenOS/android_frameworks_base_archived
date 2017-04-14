3ff63f45f255658f3c56eb85eca1eb3156389cff
Harsh Shandilya <msfjarvis@gmail.com>
2017-04-15 01:36:26 +0530

    StatusBarIconController: Ensure battery text view is tinted as well
    Change-Id: I1a1b6f6e667e5c7e2de891fbdea631d563cd6a02

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -146,6 +146,8 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mLightModeIconColorSingleTone = context.getColor(R.color.light_mode_icon_color_single_tone);
         mHandler = new Handler();
         loadDimens();
+        mBatteryLevelView = (BatteryLevelTextView) statusBar.findViewbyId(R.id.battery_level);
+
 
         TunerService.get(mContext).addTunable(this, ICON_BLACKLIST);
     }
@@ -539,6 +541,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
                 isInArea(mTintArea, mBatteryMeterView) ? mDarkIntensity : 0);
         mClock.setTextColor(getTint(mTintArea, mClock, mIconTint));
         mCarrierLabel.setTextColor(getTint(mTintArea, mCarrierLabel, mIconTint));
+        mBatteryLevelView.setTextColor(getTint(mTintArea, mBatteryLevelView, mIconTint));
     }
 
     public void appTransitionPending() {
@@ -628,3 +631,4 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         FontSizeUtils.updateFontSize(mBatteryLevelView, R.dimen.battery_level_text_size);
     }
 }
+
