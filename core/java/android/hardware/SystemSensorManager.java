6098132cf02b0f1be8355c7e848226f954084747
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-09 20:53:52 +0200

    SystemSensorManager: sensor block per-package
    Change-Id: I88ece292a6ef6867577b2e9cca5ea5e2d1d471b2

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -143,6 +143,17 @@ public class SystemSensorManager extends SensorManager {
             Log.e(TAG, "maxBatchReportLatencyUs and delayUs should be non-negative");
             return false;
         }
+        if (sensor.getType() == Sensor.TYPE_SIGNIFICANT_MOTION) {
+            String pkgName = mContext.getPackageName();
+            for (String blockedPkgName : mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_blockPackagesSensorDrain)) {
+                if (pkgName.equals(blockedPkgName)) {
+                    Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
+                                    "significant motion sensor");
+                    return false;
+                }
+            }
+        }
 
         // Invariants to preserve:
         // - one Looper per SensorEventListener
