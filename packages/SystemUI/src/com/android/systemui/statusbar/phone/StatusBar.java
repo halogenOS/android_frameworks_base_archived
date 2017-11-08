966b0e6c774843d2910afa6e5cd3b16c8f58fd7b
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-08 23:36:58 +0100

    Make the visualizer more extensible
    So that it can be used anywhere.
    
    Change-Id: Ib246a3234ffff9788a501c3abb84127e20eb5f26
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2491,8 +2491,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         if (getKeyguardBottomAreaView().mVisualizerView != null &&
-                keyguardVisible && hasArtwork &&
-                (artworkDrawable instanceof BitmapDrawable)) {
+                hasArtwork && (artworkDrawable instanceof BitmapDrawable)) {
             getKeyguardBottomAreaView().mVisualizerView
                 .setBitmap(((BitmapDrawable)artworkDrawable).getBitmap());
         }
