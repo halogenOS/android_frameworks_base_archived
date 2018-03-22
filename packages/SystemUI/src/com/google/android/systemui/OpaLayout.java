halogenOS/android_frameworks_base_archived

commit 4c450fea302dfc79a9fe958ef09dc0abee746276

Add Pixel navbar icons
xml kanged from https://github.com/DirtyUnicorns/android_packages_apps_DUI/commit/5ab126402d9b86ff08fb6a383bc9199a23cc2e8d

added also the needed dark counterpart

Change-Id: I2755795089e9dc7c83605d1f0d77b19319fcd58a
Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

packages/SystemUI/res/values/aosip_colors.xml
@@ -1,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+/*
+ * Copyright 2010, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+-->
+<resources>
+    <!-- Navbar icon Colors -->
+    <color name="icon_color_back">@android:color/white</color>
+    <color name="icon_color_ime_switcher">@android:color/white</color>
+    <color name="icon_color_split_screen">@android:color/white</color>
+    <color name="icon_color_home">@android:color/white</color>
+    <color name="icon_color_overview">@android:color/white</color>
+    <color name="icon_color_back_dark">@android:color/black</color>
+    <color name="icon_color_ime_switcher_dark">@android:color/black</color>
+    <color name="icon_color_split_screen_dark">@android:color/black</color>
+    <color name="icon_color_home_dark">@android:color/black</color>
+    <color name="icon_color_overview_dark">@android:color/black</color>
+</resources>

commit 06f60a17e8e8738a4d2006ac4bbab11235f194dc

Correct colors for Pixel navbar icons
Dark icons were never supposed to pull from framework's black color, fix that.
Also revert dcd381b21b1d ("OpaLayout: Evaluate intensity correctly").
Now the behavior is identical to a stock Pixel image

Change-Id: I972220f7a790fec03ba086a2b6ce9c5851c41a09
Fixes: 4c450fea302d ("Add Pixel navbar icons")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

packages/SystemUI/res/values/aosip_colors.xml
@@ -1,31 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-/*
- * Copyright 2010, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
--->
-<resources>
-    <!-- Navbar icon Colors -->
-    <color name="icon_color_back">@android:color/white</color>
-    <color name="icon_color_ime_switcher">@android:color/white</color>
-    <color name="icon_color_split_screen">@android:color/white</color>
-    <color name="icon_color_home">@android:color/white</color>
-    <color name="icon_color_overview">@android:color/white</color>
-    <color name="icon_color_back_dark">@android:color/black</color>
-    <color name="icon_color_ime_switcher_dark">@android:color/black</color>
-    <color name="icon_color_split_screen_dark">@android:color/black</color>
-    <color name="icon_color_home_dark">@android:color/black</color>
-    <color name="icon_color_overview_dark">@android:color/black</color>
-</resources>

packages/SystemUI/res/values/xos_colors.xml
@@ -1,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+/*
+ * Copyright 2010, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+-->
+<resources>
+    <!-- Navbar icon Colors -->
+    <color name="icon_color_back">@color/light_mode_icon_color_single_tone</color>
+    <color name="icon_color_ime_switcher">@color/light_mode_icon_color_single_tone</color>
+    <color name="icon_color_split_screen">@color/light_mode_icon_color_single_tone</color>
+    <color name="icon_color_home">@color/light_mode_icon_color_single_tone</color>
+    <color name="icon_color_overview">@color/light_mode_icon_color_single_tone</color>
+    <color name="icon_color_back_dark">@color/dark_mode_icon_color_single_tone</color>
+    <color name="icon_color_ime_switcher_dark">@color/dark_mode_icon_color_single_tone</color>
+    <color name="icon_color_split_screen_dark">@color/dark_mode_icon_color_single_tone</color>
+    <color name="icon_color_home_dark">@color/dark_mode_icon_color_single_tone</color>
+    <color name="icon_color_overview_dark">@color/dark_mode_icon_color_single_tone</color>
+</resources>

packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -94,8 +94,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context) {
         super(context);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -124,8 +124,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -154,8 +154,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -184,8 +184,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;