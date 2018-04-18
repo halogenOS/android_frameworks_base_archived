1d45a511dc423414ac369ef80d82647d60837305
Harsh Shandilya <harsh@prjkt.io>
2018-04-18 16:14:09 +0530

    WifiTile: Avoid NPE during boot phase
    * Listening handling can only happen after mController
      is properly initialised.
    
    Change-Id: I8b77abdf92fd2b23e51087d3876f92207dc2e4f1
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -140,6 +140,7 @@ public class WifiTile extends QSTileImpl<SignalState> {
 
     @Override
     protected void handleUpdateState(SignalState state, Object arg) {
+        if (mSignalCallback == null) return;
         if (DEBUG) Log.d(TAG, "handleUpdateState arg=" + arg);
         CallbackInfo cb = (CallbackInfo) arg;
         if (cb == null) {
