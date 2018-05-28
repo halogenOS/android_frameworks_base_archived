1a50320b362b11db0503397d2c7b47f5044865af
Bernie Innocenti <codewiz@google.com>
2018-05-28 22:04:37 +0900

    vpn: allow IPSec traffic through Always-on VPN
    This won't leak any traffic outside the VPN as long as there are no
    processes owned by uid 0 which generate network traffic (which is
    currently the case).
    
    Bug: 69873852
    Test: compared the output of 'adb shell ip rule show' before and after
    Test: runtest -x frameworks/base/tests/net/java/com/android/server/connectivity/VpnTest.java
    Test: local CTS tests run: android.net.cts.VpnServiceTest
    Test: local CTS tests run: com.android.cts.devicepolicy.MixedDeviceOwnerTest
    Change-Id: I8758e576c9d961d73f62bfcf0559dd7ecee6e8e6
    Merged-In: I8758e576c9d961d73f62bfcf0559dd7ecee6e8e6
    Merged-In: I1f9b78c8f828ec2df7aba71b39d62be0c4db2550
    Merged-In: I8edeb0942e661c8385ff0cd3fdb72e6f62a8f218
    (cherry picked from commit 00000fe55a4729f8339afdc7eab5c970b2549813)
    (cherry picked from commit ef2910dc709d698b6476e8d462c608d04c784a26)
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -116,8 +116,6 @@ import com.android.server.DeviceIdleInternal;
 import com.android.server.LocalServices;
 import com.android.server.net.BaseNetworkObserver;
 
-import libcore.io.IoUtils;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -145,6 +143,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import libcore.io.IoUtils;
 
 /**
  * @hide
