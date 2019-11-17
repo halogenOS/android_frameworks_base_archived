4831ffd9fd461161d1a1f3a9cf9722a3aa3a9a3b
Simao Gomes Viana <xdevs23@outlook.com>
2019-11-17 18:17:25 +0100

    AODTile/CaffeineTile: use XOS metric instead of this thing we don't have
    Change-Id: Idc84174ee7021a36c6452792dabec3c65c9d4834

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -131,7 +131,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_QUICK_TILES;
+        return MetricsEvent.XOS;
     }
 
     private void startCountDown(long duration) {
