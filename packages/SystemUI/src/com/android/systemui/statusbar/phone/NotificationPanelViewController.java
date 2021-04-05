808f10dc03d195409a9036d268ec1d6ee8df9f00
Simão Gomes Viana <devel@superboring.dev>
2021-04-05 13:57:31 +0200

    Revert "base: Double tap to sleep on lockscreen"
    does not appear to work, we'll use a more complex solution
    
    This reverts commit 7d1129674a01fb0c815004a9cfb532dbf92db9ea.
    
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -30,7 +30,6 @@ import android.animation.ValueAnimator;
 import android.app.ActivityManager;
 import android.app.Fragment;
 import android.app.StatusBarManager;
-import android.content.Context;
 import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.graphics.Canvas;
@@ -47,7 +46,6 @@ import android.os.PowerManager;
 import android.os.SystemClock;
 import android.util.Log;
 import android.util.MathUtils;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -419,8 +417,6 @@ public class NotificationPanelViewController extends PanelViewController {
     private final ShadeController mShadeController;
     private int mDisplayId;
 
-    private GestureDetector mDoubleTapGestureListener;
-
     /**
      * Cache the resource id of the theme to avoid unnecessary work in onThemeChanged.
      *
@@ -561,16 +557,6 @@ public class NotificationPanelViewController extends PanelViewController {
         });
         mBottomAreaShadeAlphaAnimator.setDuration(160);
         mBottomAreaShadeAlphaAnimator.setInterpolator(Interpolators.ALPHA_OUT);
-        mDoubleTapGestureListener = new GestureDetector(mView.getContext(),
-                new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onDoubleTap(MotionEvent event) {
-                final PowerManager pm = (PowerManager) mView.getContext().getSystemService(
-                        Context.POWER_SERVICE);
-                pm.goToSleep(event.getEventTime());
-                return true;
-            }
-        });
         mShadeController = shadeController;
         mLockscreenUserManager = notificationLockscreenUserManager;
         mEntryManager = notificationEntryManager;
@@ -3203,10 +3189,6 @@ public class NotificationPanelViewController extends PanelViewController {
                 // to pull down QS or expand the shade.
                 if (mStatusBar.isBouncerShowingScrimmed()) {
                     return false;
-                }
-
-                if (mBarState == StatusBarState.KEYGUARD) {
-                    mDoubleTapGestureListener.onTouchEvent(event);
                 }
 
                 // Make sure the next touch won't the blocked after the current ends.
