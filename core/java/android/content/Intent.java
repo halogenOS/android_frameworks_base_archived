ca9561e44ce09de59b698df17bbeb9e9929212a4
Simao Gomes Viana <simao@halogenos.org>
2022-08-27 20:08:04 +0200

    Remove rest of RECEIVE_BATTERY_CHARGE_UPDATES perm
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -6475,7 +6475,6 @@ public class Intent implements Parcelable, Cloneable {
 
     /**
      * Broadcast action: notify system apps that the battery charge level has changed
-     * Requires {@link Manifest.permission#RECEIVE_BATTERY_CHARGE_UPDATES} permission.
      * @hide
      */
     public static final String ACTION_BATTERY_CHARGE_CHANGED =
