9b170ff5213e17668b994e932676f9ca162ef186
Simão Gomes Viana <devel@superboring.dev>
2023-10-14 17:26:09 +0200

    VolumeDialogImpl: add margin to left volume panel
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -460,7 +460,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         final int[] locInWindow = new int[2];
         view.getLocationInWindow(locInWindow);
 
-        float x = mVolumePanelOnLeftSide ? 0f : locInWindow[0];
+        float x = mVolumePanelOnLeftSide ?
+            (float) mContext.getResources().getDimensionPixelSize(4) : locInWindow[0];
         float y = locInWindow[1];
 
         // The ringer and rows container has extra height at the top to fit the expanded ringer
