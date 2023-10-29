AOSP-Krypton/frameworks_base

[SQUASH]base: Introduce PixelPropsUtils
That will spoof build fingerprints on some g00gle apps

* Also will enable some cool stuffs like:
  - Dynamic navbar on GBoard
  - SafetyHub

and others.

Thanks to @kdrag0n for the original idea at https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a

Change-Id: I1078e7402833fec77edb751070c5144d08c85b6c
Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Danny Lin <danny@kdrag0n.dev>

PixelPropsUtils: Override fingerprint for SettingsIntelligence

Change-Id: Ia839345da18dde46bfd1acb77b42e0c47d28c8ac

PixelPropsUtils: Cleanup apps

Change-Id: Ida149c01cb7bf09a4275e7e436ce99ffc94a5bf0
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: bring back wallpaper collection

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
Change-Id: Ia3c11cc377107ed856fc9d9594aad45bf5a49185
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Change props to crosshatch

Change-Id: I577ceed8b54311e4255a28573f99d1a53252466a
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set pixel 5 props for specific apps

Change-Id: I36d1bc53679ea6d21cc304420b12d7500b093a2a
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Switch back to Pixel 5 props for all apps

Change-Id: I54e2c16aee37d3d18f9b7a08e2b1d09b94e8e599
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set redfin build fingerprint

Change-Id: Ib819a2f3494cffc10590638ac260cebfd8cffe79
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Set device model to Pixel 3 XL for Google

Credits to @kdrag0n. Based on https://github.com/ProtonAOSP/android_frameworks_base/commit/5078079773c75b67f883236eb60a4e520f71b43d

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

PixelPropsUtils: Update packages

Change-Id: I8c0c55e258bdf767050f07bd83e3d1df44ea98c2
Co-authored-by: jhenrique09 <jhenrique09.mcz@ho

core/java/com/android/internal/os/Zygote.java
@@ -47,6 +47,8 @@ import java.io.DataOutputStream;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
+import com.android.internal.util.krypton.PixelPropsUtils;
+
 /** @hide */
 public final class Zygote {
     /*
@@ -883,6 +885,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";

halogenOS/android_frameworks_base_archived

Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

core/java/com/android/internal/os/Zygote.java
@@ -56,6 +56,8 @@ import java.io.DataOutputStream;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
+import com.android.internal.util.yaap.PixelPropsUtils;
+
 /** @hide */
 public final class Zygote {
     /*
@@ -900,6 +902,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";